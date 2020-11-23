b19cbd6c16eaf96a0da6326d18a8f52d9b81bcae
MrgnCpn <morgancpn.dev@gmail.com>
2020-11-23 02:54:30 +0100

    Configuration class + create endpoint

--- RewardCentral/src/main/java/com/tripmaster/rewardcentral/RewardCentralController.java
+++ RewardCentral/src/main/java/com/tripmaster/rewardcentral/RewardCentralController.java
@@ -1,7 +1,28 @@
 package com.tripmaster.rewardcentral;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.jsoniter.output.JsonStream;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+import rewardCentral.RewardCentral;
+
+import java.util.UUID;
 
 @RestController
 public class RewardCentralController {
+    @Autowired
+    RewardCentral rewardCentral;
+
+    @GetMapping("/getAttractionRewardPoints")
+    public String getAttractionRewardPoints(@RequestParam(required = true) String userId, String attractionId) {
+        if (!StringUtils.isEmpty(userId) && !StringUtils.isEmpty(attractionId)) {
+            return JsonStream.serialize(
+                    rewardCentral.getAttractionRewardPoints(
+                            UUID.fromString(userId),
+                            UUID.fromString(attractionId)
+                    )
+            );
+        }
+        return null;
+    }
 }
