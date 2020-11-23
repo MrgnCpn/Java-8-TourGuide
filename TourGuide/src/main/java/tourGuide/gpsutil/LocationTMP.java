7a4e0d52261d61bcb7b0aec82d95d3c449a497d8
MrgnCpn <morgancpn.dev@gmail.com>
2020-11-23 03:46:49 +0100

    Configure and create endpoints

---
+++ TourGuide/src/main/java/tourGuide/gpsutil/LocationTMP.java
@@ -0,0 +1,11 @@
+package tourGuide.gpsutil;
+
+public class LocationTMP {
+    public final double longitude;
+    public final double latitude;
+
+    public LocationTMP(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+}
--- TripPricer/src/main/java/com/tripmaster/trippricer/TripPricerController.java
+++ TripPricer/src/main/java/com/tripmaster/trippricer/TripPricerController.java
@@ -1,7 +1,20 @@
 package com.tripmaster.trippricer;
 
+import com.jsoniter.output.JsonStream;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class TripPricerController {
+    @Autowired
+    private TripPricerService tripPricerService;
+
+    @PostMapping("/getPrice")
+    public String getPrice(@RequestParam Map<String, Object> requestParams) {
+        return JsonStream.serialize(tripPricerService.getPrice(requestParams));
+    }
 }
